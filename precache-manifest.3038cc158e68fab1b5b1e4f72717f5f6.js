b27da15641ab44e9e6e62a45ce4d8a00872fe107
Ankit-0212 <ankitrajsrivastava222@gmail.com>
2022-05-08 13:17:01 +0530

    Updates

---
+++ precache-manifest.3038cc158e68fab1b5b1e4f72717f5f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cbe97364fa2f8470a1086de384e801b",
+    "url": "/AlanAINewsAPP/index.html"
+  },
+  {
+    "revision": "ebb99484d85fdb13b75a",
+    "url": "/AlanAINewsAPP/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "ae8997939b3d414e0522",
+    "url": "/AlanAINewsAPP/static/js/2.c0fef7a6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/AlanAINewsAPP/static/js/2.c0fef7a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebb99484d85fdb13b75a",
+    "url": "/AlanAINewsAPP/static/js/main.e86414af.chunk.js"
+  },
+  {
+    "revision": "e7dbbf11e40712d719e0",
+    "url": "/AlanAINewsAPP/static/js/runtime-main.0edbbff8.js"
+  }
+]);
